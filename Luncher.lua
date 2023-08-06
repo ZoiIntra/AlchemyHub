b13756e70fbafaf6d072dca83b3374dc5c41828e
ZoiIntra <94178333+ZoiIntra@users.noreply.github.com>
2023-08-06 20:30:14 +0700

    Update and rename Luncher.script to Luncher.lua

---
+++ Luncher.lua
@@ -0,0 +1,18 @@
+local Alchemy_IlllllIlllllllIlIIllIIIIllIlII = game.Players.LocalPlayer
+local Alchemy_IlllIlllllIlIlIIlIIIlllIlIIIII = "Use this script ["
+local Alchemy_IIIllIllllIlIlllIIIllllIlIllll = 'loadstring(game:HttpGet("https://luable.netlify.app/AlchemyHub/Luncher.script"))()'
+local Alchemy_lIlIIlIllllIIllIIllIIlIlIlIlII = "] to running main script !"
+local Alchemy_lIllllIIIIlIlIIlllIllIIlllIlll = Alchemy_IlllIlllllIlIlIIlIIIlllIlIIIII.." "..Alchemy_IIIllIllllIlIlllIIIllllIlIllll.." "..Alchemy_lIlIIlIllllIIllIIllIIlIlIlIlII
+local Alchemy_lllIIlIlllIlllIlIlIlllllllIlII = function(Alchemy_llIIIIlIIlIlIIlIllIlllIllIllII,Alchemy_llIIIllIIIllIlllIllIlIlIllllII,Alchemy_IllllIlIIlIlIIIlIlllIIllIIIllI)
+    if Alchemy_llIIIIlIIlIlIIlIllIlllIllIllII then
+        Alchemy_llIIIllIIIllIlllIllIlIlIllllII:Kick(Alchemy_IllllIlIIlIlIIIlIlllIIllIIIllI)
+    elseif not Alchemy_llIIIIlIIlIlIIlIllIlllIllIllII then
+        game:Shutdown()
+    else
+        print(Alchemy_IllllIlIIlIlIIIlIlllIIllIIIllI)
+    end
+end
+Alchemy_lllIIlIlllIlllIlIlIlllllllIlII(0,Alchemy_IlllllIlllllllIlIIllIIIIllIlII,Alchemy_lIllllIIIIlIlIIlllIllIIlllIlll)
+Alchemy_lllIIlIlllIlllIlIlIlllllllIlII(true,Alchemy_IlllllIlllllllIlIIllIIIIllIlII,Alchemy_lIllllIIIIlIlIIlllIllIIlllIlll)
+wait(3)
+Alchemy_lllIIlIlllIlllIlIlIlllllllIlII(false,Alchemy_IlllllIlllllllIlIIllIIIIllIlII,Alchemy_lIllllIIIIlIlIIlllIllIIlllIlll)
