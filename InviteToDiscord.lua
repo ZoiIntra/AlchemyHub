5497fdfef3f114ac29aa5a35fae47a5cb2255492
x2Neptune <94178333+ZoiIntra@users.noreply.github.com>
2023-11-15 19:42:50 +0700

    Update InviteToDiscord.lua

--- InviteToDiscord.lua
+++ InviteToDiscord.lua
@@ -1,1 +1,7 @@
+local SlayerzUI_Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2-Neptune/SlayerzUI/main/Notification.script"))()
+local NameHub = "Alchemy Hub"
+local Description = "If UI not load please rejoin!!!"
+local Logo = "rbxassetid://14981376704"
+local Color = Color3.fromRGB(0, 255, 145)
+SlayerzUI_Notify:Notify(NameHub, Description, Logo, Color, 5)
 print("Success!")
