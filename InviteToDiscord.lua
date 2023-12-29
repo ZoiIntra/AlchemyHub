ecb60147f2a79681cb6c184ea386d7a35ed0e172
x2Neptune <94178333+ZoiIntra@users.noreply.github.com>
2023-12-29 16:22:13 +0700

    Update InviteToDiscord.lua

--- InviteToDiscord.lua
+++ InviteToDiscord.lua
@@ -1,6 +1,6 @@
 local SlayerzUI_Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2-Neptune/SlayerzUI/main/Notification.script"))()
 local NameHub = "Alchemy Hub"
-local Description = ""
+local Description = "Merry Christmas"
 local Logo = "rbxassetid://14981376704"
 local Color = Color3.fromRGB(0, 255, 145)
 SlayerzUI_Notify:Notify(NameHub, Description, Logo, Color, 5)
